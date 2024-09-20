208a25a0960818019af81175fd95fa7c63b5aa9a
Vlad Korenev <korenevlad17@gmail.com>
2024-09-20 22:52:29 +0300

    init

---
+++ 1_BaseType/1_WorkingWithNumbers.js
@@ -0,0 +1,13 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+const n1 = 40;
+const n2 = 2.56;
+let a = 12;
+let b = Infinity;
+let c = NaN;
+//let d: 42 = 24 // error
+let e = 0x1;
+let f = 0.1;
+console.log(sum(n1, n2));
